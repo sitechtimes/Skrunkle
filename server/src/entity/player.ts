a509b11d85562a2f4f5c3b6bfb39a356426a84ab
ZHEN <YOUWEI.ZHEN@C56.local>
2022-09-30 12:33:06 -0400

    entity

--- server/src/entity/player.ts
+++ server/src/entity/player.ts
@@ -1,9 +1,21 @@
 import { Vector3 } from "babylonjs"
 
 export class Player{
-    private position: Vector3;
 
-    constructor(position: Vector3){
+    private _name: string;
+    private _health: number;
+    private _level: number;
+    private _position: Vector3;
 
+    constructor(name: string, health: number, level: number, position: Vector3){
+        this._name = name;
+        this._health = health;
+        this._level = level;
+        this._position = position;
     }
+
+    public get position(): Vector3{
+        return this._position;
+    }
+
 }
