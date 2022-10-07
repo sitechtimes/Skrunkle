5c150b54274ee3e6bb95414a43786f3a322f1938
ANDERSON <THOMAS.ANDERSON@C56.local>
2022-10-07 12:38:06 -0400

    need to make mirrors

--- client/main.js
+++ client/main.js
@@ -4,7 +4,7 @@ const createScene = function () {
   const scene = new BABYLON.Scene(engine);
   const sphere = BABYLON.MeshBuilder.CreateSphere(
     "sphere",
-    { diameter: 2, segments: 32 },
+    { diameter: 5, segments: 32 },
     scene
   );
   var camera = new BABYLON.FreeCamera(
@@ -12,6 +12,23 @@ const createScene = function () {
     new BABYLON.Vector3(0, 25, 0),
     scene
   );
+  var cylinder = new BABYLON.MeshBuilder.CreateCylinder("cylinder", {
+    diameter: 1,
+    segments: 3,
+  });
+
+  var mirrorMaterial = new BABYLON.StandardMaterial("texture4", scene);
+
+  mirrorMaterial.reflectionTexture = new BABYLON.MirrorTexture(
+    "mirror",
+    512,
+    scene,
+    true
+  ); //Create a mirror texture
+
+  mirrorMaterial.reflectionTexture.renderList = [cylinder, ground0];
+  mirrorMaterial.reflectionTexture.level = 1; //Select the level (0.0 > 1.0) of the reflection
+  sphere.material = mirrorMaterial;
   camera.attachControl(canvas, true);
   camera.keysUp = [87];
   camera.keysDown = [83];
@@ -29,11 +46,19 @@ const createScene = function () {
   camera.ellipsoid = new BABYLON.Vector3(0.25, 1.5, 0.25);
   camera._needMoveForGravity = true;
   camera.position.y = 1;
-  camera.parent = sphere;
-  sphere.position.y = 3;
-  // Move the sphere upward 1/2 its height
+  camera.position.y = 3;
+  sphere.position.y = 4;
+  // var followBehavior = new BABYLON.FollowBehavior();
+  // followBehavior.attach(cylinder);
+  // followBehavior.maximumDistance = -1;
+  // followBehavior.lerpTime = 0;
+  // followBehavior.maxViewVerticalDegrees = 0;
+  // followBehavior.maxViewHorizontalDegrees = 0;
+  // followBehavior.ignoreCameraPitchAndRoll = true;
+  // // Move the sphere upward 1/2 its height
 
   // Our built-in 'ground' shape.
+
   var ground0 = BABYLON.Mesh.CreateGround("ground0", 250, 250, 2, scene);
   ground0.checkCollisions = true;
   var light = new BABYLON.HemisphericLight(
@@ -53,15 +78,6 @@ const createScene = function () {
 
       case 16: // Shift
         speed = 5;
-        break;
-
-      case 82: // R
-        if (care == true && aure == 0) {
-          aure = 100;
-          akm.position = new BABYLON.Vector3(0.5, -0.7, 0.5);
-          camera.fov = 1.5;
-          reload.play();
-        }
         break;
     }
   };
@@ -105,15 +121,10 @@ const createScene = function () {
       canvas.removeEventListener("mousemove", mousemoveCallback, false);
     }
   }
-
-  function mousemoveCallback(event) {
-    var demo = requestAnimationFrame(mousemoveCallback);
-  }
   var wj = true;
   var grav = -0.6;
   var speed = 2.5;
   var spood = 0;
-  var care = true;
   var onKeyDown = function (event) {
     switch (event.keyCode) {
       case 32: // Space
@@ -126,15 +137,6 @@ const createScene = function () {
 
       case 16: // Shift
         speed = 5;
-        break;
-
-      case 82: // R
-        if (care == true && aure == 0) {
-          aure = 100;
-          akm.position = new BABYLON.Vector3(0.5, -0.7, 0.5);
-          camera.fov = 1.5;
-          reload.play();
-        }
         break;
     }
   };
@@ -161,26 +163,28 @@ const createScene = function () {
     canvas.removeEventListener("keydown", onKeyDown);
     canvas.removeEventListener("keyup", onKeyUp);
   };
-  var update = function () {
+  const update = function () {
     camera.speed = speed + spood;
-    console.out(dT);
-
     camera.onCollide = function (colMesh) {
       if (colMesh.uniqueId === ground0.uniqueId) {
         wj = true;
       }
     };
-
     scene.gravity.y = grav;
-
     if (grav > -0.4) {
       grav -= 0.05;
     }
   };
 
+  scene.registerBeforeRender(function () {
+    update();
+  });
+  cylinder.position = camera.position;
+  camera.fov = 360;
   return scene;
 };
 const scene = createScene(); //Call the createScene function
+
 // Register a render loop to repeatedly render the scene
 engine.runRenderLoop(function () {
   scene.render();
