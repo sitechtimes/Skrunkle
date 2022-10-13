ef6067a590371cbc66b578e574bfd9a83beabb08
ZHEN <YOUWEI.ZHEN@C56.local>
2022-10-13 12:29:41 -0400

    refactpromg world

---
+++ client/entity/player.ts
@@ -0,0 +1,70 @@
+import { Vector3, UniversalCamera, Mesh } from "babylonjs"
+
+export class Player{
+
+    private _name: string;
+    private _health: number;
+    private _exp: number;
+    private _position: Vector3;
+    private _id: string;
+    private _camera: UniversalCamera;
+    private _body: Mesh;
+
+    constructor(
+        name: string, 
+        health: number, 
+        exp: number, 
+        position: Vector3, 
+        id?: string|undefined
+    ){
+        this._name = name;
+        this._health = health;
+        this._exp = exp;
+        this._position = position;
+        this._id = id;
+
+        this.position = this._position;
+    }
+
+    public get position(): Vector3{
+        return this._position;
+    }
+
+    public set position(new_position: Vector3){
+        this._position = new_position;
+        this._camera.position = this._position;
+        this._body.position = this._position;
+    }
+
+    public get name(): string{
+        return this._name;
+    }
+
+    public set name(new_name: string){
+        this._name = new_name;
+    }
+
+    public get health(): number{
+        return this._health;
+    }
+
+    public set health(new_health: number){
+        if (new_health < 0 || new_health > 100){
+            throw new Error("Player entity health out of bound")
+        }
+        this._health = new_health;
+    }
+
+    public get exp(): number{
+        return this._exp;
+    }
+
+    public set exp(new_exp: number){
+        this._exp = new_exp;
+    }
+
+    public get id(): string{
+        return this._id;
+    }
+
+}
