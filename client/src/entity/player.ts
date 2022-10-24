c44802ef6fa68cda9bc3db526f80fc59be9e79d7
Anthony Zhen <anthony20151128@gmail.com>
2022-10-24 13:07:41 -0400

    added new player body:

--- client/src/entity/player.ts
+++ client/src/entity/player.ts
@@ -1,24 +1,24 @@
-import { Mesh, MeshBuilder, Scene, Vector3 } from "babylonjs"
+import { Mesh, MeshBuilder, Scene, Vector3, SceneLoader, TransformNode } from "babylonjs"
 
-export class Player{
+export class Player {
 
     private _name: string;
     private _health: number;
     private _exp: number;
     private _position: Vector3;
     private _id: string;
-    private _body: Mesh | null = null;
+    private _body: Mesh | TransformNode | null = null;
     private _scene: Scene;
 
     constructor(
-        name: string, 
-        health: number, 
-        exp: number, 
-        position: Vector3, 
+        name: string,
+        health: number,
+        exp: number,
+        position: Vector3,
         id: string,
         scene: Scene,
-        options: { renderBody?: boolean } = { renderBody: true}
-    ){
+        options: { renderBody?: boolean } = { renderBody: true }
+    ) {
         this._name = name;
         this._health = health;
         this._exp = exp;
@@ -26,50 +26,62 @@ export class Player{
         this._scene = scene
 
         if (options.renderBody) {
-            this._body = MeshBuilder.CreateBox("playerBody", { size: 5, width: 5, height: 7}, this._scene)
+            SceneLoader.ImportMesh("", "meshes/", "player.babylon", this._scene, this._init)
+            console.log(this._body)
         }
         this._position = position;
     }
 
-    public get position(): Vector3{
+    private _init(scene: any) {
+        let parent: TransformNode = new TransformNode("grouped-mesh")
+        for (let child of scene) {
+            child.parent = parent
+        }
+        parent.position = new Vector3(0, 3, 0)
+        parent.rotation = new Vector3(Math.PI / 2, 0, 0)
+        parent.scaling = new Vector3(0.25, 0.25, 0.25)
+        this._body = parent
+    }
+
+    public get position(): Vector3 {
         return this._position;
     }
 
-    public set position(new_position: Vector3){
+    public set position(new_position: Vector3) {
         this._position = new_position;
         if (this._body) {
             this._body.position = this._position;
         }
     }
 
-    public get name(): string{
+    public get name(): string {
         return this._name;
     }
 
-    public set name(new_name: string){
+    public set name(new_name: string) {
         this._name = new_name;
     }
 
-    public get health(): number{
+    public get health(): number {
         return this._health;
     }
 
-    public set health(new_health: number){
-        if (new_health < 0 || new_health > 100){
+    public set health(new_health: number) {
+        if (new_health < 0 || new_health > 100) {
             throw new Error("Player entity health out of bound")
         }
         this._health = new_health;
     }
 
-    public get exp(): number{
+    public get exp(): number {
         return this._exp;
     }
 
-    public set exp(new_exp: number){
+    public set exp(new_exp: number) {
         this._exp = new_exp;
     }
 
-    public get id(): string{
+    public get id(): string {
         return this._id;
     }
 
@@ -79,7 +91,7 @@ export class Player{
         }
     }
 
-    protected get scene(): Scene{
+    protected get scene(): Scene {
         return this._scene
     }
 
--- client/src/world/world.ts
+++ client/src/world/world.ts
@@ -23,17 +23,7 @@ export class World {
     public init(): void {
         // Camera is absolutely needed, for some reason BabylonJS requires a camera for Server or will crash
         this._playerCamera = new FreeCamera("FreeCamera", new Vector3(0, 20, 0), this._scene);
-        SceneLoader.ImportMesh("","gltf/", "player.babylon", this._scene, function (scene) {
-            // do something with the scene
-            let parent: TransformNode = new TransformNode("grouped-mesh")
-            for (let child of scene){
-                child.parent = parent
-            }
-            parent.position = new Vector3(0, 3, 0)
-            parent.rotation = new Vector3(Math.PI/2, 0, 0)
-            parent.scaling = new Vector3(0.25, 0.25, 0.25)
-        });
-        var ground = MeshBuilder.CreateGround("ground", { width: 100, height: 100 }, this._scene);
+        var ground = MeshBuilder.CreateGround("ground", { width: 500, height: 500 }, this._scene);
         ground.checkCollisions = true;
         var light = new HemisphericLight(
             "light",
