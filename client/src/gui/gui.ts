1ab58edc9f4521d167e130044cbde06562d46f5d
ewright516 <ewright51606@gmail.com>
2022-11-01 18:35:12 -0400

    small changes

--- client/src/gui/gui.ts
+++ client/src/gui/gui.ts
@@ -1,9 +1,8 @@
-import { AdvancedDynamicTexture, Button } from "@babylonjs/gui"
-import { Scene } from "babylonjs"
+import { AdvancedDynamicTexture } from "@babylonjs/gui"
+import { Scene } from "@babylonjs/core"
 
 export class GUI {
   private _mainGUI: AdvancedDynamicTexture
-  private _loadedGUI
   private _scene: Scene
 
   constructor(scene: Scene) {
@@ -11,22 +10,11 @@ export class GUI {
     this._mainGUI = AdvancedDynamicTexture.CreateFullscreenUI("main-gui", true, this._scene)
   }
 
-  public async createMenu() {
-    /* const button1 = Button.CreateSimpleButton('but1', 'Click Me')
-    button1.width = "150px"
-    button1.height = "40px";
-    button1.color = "white";
-    button1.cornerRadius = 20;
-    button1.background = "green";
-    button1.onPointerUpObservable.add(() => {
-      this._mainGUI.removeControl(button1)
-    });
-    this._mainGUI.addControl(button1);  */
-
+  public async createHotbar() {
     await this._mainGUI.parseFromSnippetAsync("UW33M7#8")
   }
 
-  public destroyGUI() {
-
+  public disposeGUI() {
+    this._mainGUI.dispose()
   }
 }
