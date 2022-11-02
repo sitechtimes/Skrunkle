d136667ee1ff038cb6984fd3581fdcd7c839c5d3
ewright516 <65744465+ewright516@users.noreply.github.com>
2022-11-02 12:30:59 -0400

    creating discrete hotbar

--- client/src/gui/gui.ts
+++ client/src/gui/gui.ts
@@ -1,17 +1,23 @@
 import { AdvancedDynamicTexture } from "@babylonjs/gui"
 import { Scene } from "@babylonjs/core"
+import { Hotbar } from "./hotbar"
 
 export class GUI {
   private _mainGUI: AdvancedDynamicTexture
   private _scene: Scene
+  private _hotbar: Hotbar
 
   constructor(scene: Scene) {
     this._scene = scene
     this._mainGUI = AdvancedDynamicTexture.CreateFullscreenUI("main-gui", true, this._scene)
+    this._hotbar = new Hotbar(this._mainGUI)
   }
 
   public async createHotbar() {
-    await this._mainGUI.parseFromSnippetAsync("UW33M7#8")
+    await this._hotbar.init()
+    setTimeout(() => {
+      this._hotbar.increment()
+    }, 5000)
   }
 
   public disposeGUI() {
