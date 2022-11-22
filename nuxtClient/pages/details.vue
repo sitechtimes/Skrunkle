2a87c3d40720286aef72122319e428c8b4109c7a
ashash <90792417+ashleychchan@users.noreply.github.com>
2022-11-22 12:31:06 -0500

    tog

--- nuxtClient/pages/details.vue
+++ nuxtClient/pages/details.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="body">
-    <div class="wordLogo">
-      <img src="~/assets/skrunkleWord.png" alt="Skrunkle" />
+    <div class="wrap">
+      <div class="wordLogo">
+        <img src="~/assets/skrunkleWord.png" alt="Skrunkle" />
+      </div>
+      <label class="switch">
+        <input type="checkbox" />
+        <span class="slider round"></span>
+      </label>
     </div>
     <div class="small">
       <div class="characterCircle">
@@ -99,5 +105,72 @@ label {
   position: absolute;
   top: 40%;
   right: 61%;
+}
+/* The switch - the box around the slider */
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+}
+
+/* Hide default HTML checkbox */
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+/* The slider */
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: black;
+  -webkit-transition: 0.4s;
+  transition: 0.4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 26px;
+  width: 26px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  -webkit-transition: 0.4s;
+  transition: 0.4s;
+}
+
+input:checked + .slider {
+  background-color: #fd551b;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #fd551b;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(26px);
+  -ms-transform: translateX(26px);
+  transform: translateX(26px);
+}
+
+/* Rounded sliders */
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
+}
+.wrap {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
 }
 </style>
