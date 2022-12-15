e4ab15128f25c5268a8f1aeb78f9dc3b8511b451
ashash <90792417+ashleychchan@users.noreply.github.com>
2022-12-15 12:32:03 -0500

    v

--- nuxtClient/pages/intro.vue
+++ nuxtClient/pages/intro.vue
@@ -1,5 +1,47 @@
 <template>
   <html lang="en">
-    <body></body>
+    <body>
+      <div class="box1">
+        <video
+          id="background-video"
+          autoplay
+          loop
+          muted
+          poster="https://assets.codepen.io/6093409/river.jpg"
+        >
+          <source
+            src="https://assets.codepen.io/6093409/river.mp4"
+            type="video/mp4"
+          />
+        </video>
+        <h1>SKRUNKLE</h1>
+      </div>
+      <div class="box2"></div>
+      <div class="box3"></div>
+      <div class="box4"></div>
+      <div class="box5"></div>
+    </body>
   </html>
 </template>
+<style>
+html,
+body,
+* {
+  overflow: hidden;
+  padding: 0;
+  margin: 0;
+  font-family: "Titan One", cursive;
+  box-sizing: border-box;
+}
+#background-video {
+  height: 100vh;
+  width: 100vw;
+  object-fit: cover;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  z-index: -1;
+}
+</style>
