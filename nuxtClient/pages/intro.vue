1522d57fc09585db561e3636887a915444f3461b
ashash <90792417+ashleychchan@users.noreply.github.com>
2022-12-16 12:29:22 -0500

    g

--- nuxtClient/pages/intro.vue
+++ nuxtClient/pages/intro.vue
@@ -2,24 +2,55 @@
   <html lang="en">
     <body>
       <div class="box1">
-        <video
-          id="background-video"
-          autoplay
-          loop
-          muted
-          poster="https://assets.codepen.io/6093409/river.jpg"
-        >
-          <source
-            src="https://assets.codepen.io/6093409/river.mp4"
-            type="video/mp4"
-          />
-        </video>
+        <!--  <video id="background-video" autoplay loop muted>
+          <source src="~/assets/treeBacco.mp4" type="video/mp4" />
+        </video> -->
         <h1>SKRUNKLE</h1>
+        <h2>Go getem</h2>
+        <button>play now</button>
       </div>
-      <div class="box2"></div>
-      <div class="box3"></div>
-      <div class="box4"></div>
-      <div class="box5"></div>
+      <div class="box2">
+        <h2>We are Skrunkle</h2>
+        <h3>
+          Use tactical strategies to stay alive amidst all the generated
+          obstacles. But, you'll need to think faster than your opponent if you
+          want to survive. The game introduces new game play elements to realize
+          the thrill and hardships of maintaining life in a hardcore world.
+        </h3>
+      </div>
+      <div class="box3">
+        <h2>GET READY. THEN FIGHT BACK</h2>
+        <h3>
+          Experience an explosive game of cat and mouse set in a huge open
+          world. In this reimagining of 1980’s Sweden, hostile machines have
+          invaded the serene countryside, and you need to fight back while
+          unravelling the mystery of what is really going on. By utilizing
+          battle tested guerilla tactics, you’ll be able to lure, cripple, or
+          destroy enemies in intense, creative sandbox skirmishes.
+        </h3>
+      </div>
+      <div class="box4">
+        <h2>GET READY. THEN FIGHT BACK</h2>
+        <h3>
+          Experience an explosive game of cat and mouse set in a huge open
+          world. In this reimagining of 1980’s Sweden, hostile machines have
+          invaded the serene countryside, and you need to fight back while
+          unravelling the mystery of what is really going on. By utilizing
+          battle tested guerilla tactics, you’ll be able to lure, cripple, or
+          destroy enemies in intense, creative sandbox skirmishes.
+        </h3>
+      </div>
+      <div class="box5">
+        <h2>GET READY. THEN FIGHT BACK</h2>
+        <h3>
+          Experience an explosive game of cat and mouse set in a huge open
+          world. In this reimagining of 1980’s Sweden, hostile machines have
+          invaded the serene countryside, and you need to fight back while
+          unravelling the mystery of what is really going on. By utilizing
+          battle tested guerilla tactics, you’ll be able to lure, cripple, or
+          destroy enemies in intense, creative sandbox skirmishes.
+        </h3>
+      </div>
     </body>
   </html>
 </template>
@@ -30,10 +61,11 @@ body,
   overflow: hidden;
   padding: 0;
   margin: 0;
-  font-family: "Titan One", cursive;
+
   box-sizing: border-box;
+  color: white;
 }
-#background-video {
+/* #background-video {
   height: 100vh;
   width: 100vw;
   object-fit: cover;
@@ -43,5 +75,14 @@ body,
   top: 0;
   bottom: 0;
   z-index: -1;
+} */
+.box2 {
+  background-image: url("assets/bac1.jpg");
+}
+.box3 {
+  background-image: url("assets/bac2.jpg");
+}
+.box4 {
+  background-image: url("assets/pattern2.png");
 }
 </style>
