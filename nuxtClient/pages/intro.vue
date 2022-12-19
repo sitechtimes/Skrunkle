b1e182f2d0c7c40f3166ba05fd76835ac57c6547
ashash <90792417+ashleychchan@users.noreply.github.com>
2022-12-19 12:31:32 -0500

    .

--- nuxtClient/pages/intro.vue
+++ nuxtClient/pages/intro.vue
@@ -1,55 +1,25 @@
 <template>
   <html lang="en">
     <body>
-      <div class="box1">
-        <!--  <video id="background-video" autoplay loop muted>
-          <source src="~/assets/treeBacco.mp4" type="video/mp4" />
-        </video> -->
-        <h1>SKRUNKLE</h1>
-        <h2>Go getem</h2>
-        <button>play now</button>
-      </div>
-      <div class="box2">
-        <h2>We are Skrunkle</h2>
-        <h3>
-          Use tactical strategies to stay alive amidst all the generated
-          obstacles. But, you'll need to think faster than your opponent if you
-          want to survive. The game introduces new game play elements to realize
-          the thrill and hardships of maintaining life in a hardcore world.
-        </h3>
-      </div>
-      <div class="box3">
-        <h2>GET READY. THEN FIGHT BACK</h2>
-        <h3>
-          Experience an explosive game of cat and mouse set in a huge open
-          world. In this reimagining of 1980’s Sweden, hostile machines have
-          invaded the serene countryside, and you need to fight back while
-          unravelling the mystery of what is really going on. By utilizing
-          battle tested guerilla tactics, you’ll be able to lure, cripple, or
-          destroy enemies in intense, creative sandbox skirmishes.
-        </h3>
-      </div>
-      <div class="box4">
-        <h2>GET READY. THEN FIGHT BACK</h2>
-        <h3>
-          Experience an explosive game of cat and mouse set in a huge open
-          world. In this reimagining of 1980’s Sweden, hostile machines have
-          invaded the serene countryside, and you need to fight back while
-          unravelling the mystery of what is really going on. By utilizing
-          battle tested guerilla tactics, you’ll be able to lure, cripple, or
-          destroy enemies in intense, creative sandbox skirmishes.
-        </h3>
-      </div>
-      <div class="box5">
-        <h2>GET READY. THEN FIGHT BACK</h2>
-        <h3>
-          Experience an explosive game of cat and mouse set in a huge open
-          world. In this reimagining of 1980’s Sweden, hostile machines have
-          invaded the serene countryside, and you need to fight back while
-          unravelling the mystery of what is really going on. By utilizing
-          battle tested guerilla tactics, you’ll be able to lure, cripple, or
-          destroy enemies in intense, creative sandbox skirmishes.
-        </h3>
+      <div class="container">
+        <div class="container box1">
+          <!-- <video id="background-video" autoplay loop muted>
+            <source src="~/assets/treeBacco.mp4" type="video/mp4" />
+          </video> -->
+          <h1>SKRUNKLE</h1>
+          <h2>Go getem</h2>
+          <button>play now</button>
+        </div>
+        <div class="container box2">
+          <h2>title</h2>
+          <h3>
+            Use tactical strategies to stay alive amidst all the generated
+            obstacles. But, you'll need to think faster than your opponent if
+            you want to survive. The game introduces new game play elements to
+            realize the thrill and hardships of maintaining life in a hardcore
+            world.
+          </h3>
+        </div>
       </div>
     </body>
   </html>
@@ -61,9 +31,7 @@ body,
   overflow: hidden;
   padding: 0;
   margin: 0;
-
   box-sizing: border-box;
-  color: white;
 }
 /* #background-video {
   height: 100vh;
@@ -76,8 +44,25 @@ body,
   bottom: 0;
   z-index: -1;
 } */
+.container {
+  text-align: center;
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-image: url("assets/bac3.jpg");
+  background-size: cover;
+  background-position: center;
+  background-attachment: fixed;
+}
+.box1 {
+}
 .box2 {
   background-image: url("assets/bac1.jpg");
+  background-attachment: fixed;
+  height: 1200px;
 }
 .box3 {
   background-image: url("assets/bac2.jpg");
