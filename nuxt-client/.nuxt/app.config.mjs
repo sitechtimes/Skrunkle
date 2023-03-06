928df4099a6746c03cbfa116bf9d407061fde172
Thomas-Anderson2024 <90661563+Thomas-Anderson2024@users.noreply.github.com>
2023-03-06 12:25:06 -0500

    meshes for sword, slingshot,  ore, and spork

---
+++ nuxt-client/.nuxt/app.config.mjs
@@ -0,0 +1,8 @@
+
+import { defuFn } from 'C:/Users/thomas.anderson/Documents/babylon/nuxt-client/node_modules/defu/dist/defu.mjs'
+
+const inlineConfig = {}
+
+
+
+export default defuFn(inlineConfig)
---
+++ nuxt-client/.nuxt/dist/server/client.manifest.mjs
@@ -0,0 +1,1 @@
+export { default } from "file:///C:/Users/thomas.anderson/Documents/babylon/nuxt-client/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
