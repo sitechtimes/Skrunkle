d58437a67a6544140447e70c58b7be637587a1d2
ewright516 <65744465+ewright516@users.noreply.github.com>
2023-04-19 12:31:13 -0400

    new state machine section for items, not entities

--- nuxt-client/src/state_machine.ts
+++ nuxt-client/src/state_machine.ts
@@ -15,77 +15,80 @@ class State_machine {
   public entities: Map<string, Entities> = new Map();
   private _client: MainPlayer
 
-    public players: Map<string, Player> = new Map();
-    public entities: Map<string, Entities> = new Map(); 
-    public old_entities: Map<string, Old_Entity> = new Map();
-    public sun_light: PointLight;
-    public moon_light: PointLight;
-    public shadowGenerator: ShadowGenerator;
-    private socket_ref: Socket;
-    // private world_ref: World;
+  public players: Map<string, Player> = new Map();
+  public entities: Map<string, Entities> = new Map(); 
+  public old_entities: Map<string, Old_Entity> = new Map();
+  public sun_light: PointLight;
+  public moon_light: PointLight;
+  public shadowGenerator: ShadowGenerator;
+  private socket_ref: Socket;
+  // private world_ref: World;
 
-    private ready(){
-      console.log("Checking status of State Machine")
-      if (this.socket_ref) console.log("State Machine has Socket!")
-      // if (this.world_ref) console.log("State Machine has World!")
-      // if (this.socket_ref && this.world_ref) {
-      //     console.log("State Machine is ready!")
-      // }
-    }
-    
+  // dealing with consumables
+  private items: Array<{item: PlayerItem, position: Vector3}> = []
 
-    private pass_changes(a: Entities, b: Old_Entity): boolean{
-        
-      let pos_change: Vector3 = new Vector3(a.position.x - b.position.x, a.position.y - b.position.y, a.position.z - b.position.z)
-      let rot_change: Vector3 = a.angularVelocity.subtract(b.angularVelocity);
+  private ready(){
+    console.log("Checking status of State Machine")
+    if (this.socket_ref) console.log("State Machine has Socket!")
+    // if (this.world_ref) console.log("State Machine has World!")
+    // if (this.socket_ref && this.world_ref) {
+    //     console.log("State Machine is ready!")
+    // }
+  }
 
-      let flag: boolean = true;
 
-      if (pos_change.x > smallest_pos_change && 
-          pos_change.y > smallest_pos_change && 
-          pos_change.z > smallest_pos_change
-      )   flag = false;
+  private pass_changes(a: Entities, b: Old_Entity): boolean{
+      
+    let pos_change: Vector3 = new Vector3(a.position.x - b.position.x, a.position.y - b.position.y, a.position.z - b.position.z)
+    let rot_change: Vector3 = a.angularVelocity.subtract(b.angularVelocity);
 
-      if (rot_change.x > smallest_angle_change && 
-          rot_change.y > smallest_angle_change &&
-          rot_change.z > smallest_angle_change
-      )   flag = false;
+    let flag: boolean = true;
 
-      b.update(a)
-      this.old_entities.set(b.id, b)
+    if (pos_change.x > smallest_pos_change && 
+        pos_change.y > smallest_pos_change && 
+        pos_change.z > smallest_pos_change
+    )   flag = false;
 
-      return flag
-    }
+    if (rot_change.x > smallest_angle_change && 
+        rot_change.y > smallest_angle_change &&
+        rot_change.z > smallest_angle_change
+    )   flag = false;
 
-    public check_entity(info: boolean = false): void{
-        for (let uid of this.entities.keys()){
-            let entity: Entities = this.entities.get(uid);
-            let entity_old: Old_Entity = this.old_entities.get(uid)
+    b.update(a)
+    this.old_entities.set(b.id, b)
 
-            let passed: boolean = this.pass_changes(entity, entity_old) // if changes a lot asks for reconfirm
-            
-            if (!passed) {
-                // request for position of mesh to server
-                this.socket_ref.send(new Packet(PacketType.request_mesh, [], entity.id))
-            }
-        }
-    }
+    return flag
+  }
 
-    public setSocket(socket_ref: Socket): void{
-        this.socket_ref = socket_ref;
-        this.ready()
-    }
+  public check_entity(info: boolean = false): void{
+      for (let uid of this.entities.keys()){
+          let entity: Entities = this.entities.get(uid);
+          let entity_old: Old_Entity = this.old_entities.get(uid)
 
-    public setShadowGenerator(light: IShadowLight, sun_light: PointLight, moon_light: PointLight): void{
-        this.shadowGenerator = new ShadowGenerator(1024, light);
-        this.sun_light = sun_light;
-        this.moon_light = moon_light
-        this.shadowGenerator.useBlurExponentialShadowMap = true;
-    }
+          let passed: boolean = this.pass_changes(entity, entity_old) // if changes a lot asks for reconfirm
+          
+          if (!passed) {
+              // request for position of mesh to server
+              this.socket_ref.send(new Packet(PacketType.request_mesh, [], entity.id))
+          }
+      }
+  }
 
-    public applyShadow(mesh: Mesh): void{
-        this.shadowGenerator.addShadowCaster(mesh, [this.sun_light, this.moon_light])
-    }
+  public setSocket(socket_ref: Socket): void{
+      this.socket_ref = socket_ref;
+      this.ready()
+  }
+
+  public setShadowGenerator(light: IShadowLight, sun_light: PointLight, moon_light: PointLight): void{
+      this.shadowGenerator = new ShadowGenerator(1024, light);
+      this.sun_light = sun_light;
+      this.moon_light = moon_light
+      this.shadowGenerator.useBlurExponentialShadowMap = true;
+  }
+
+  public applyShadow(mesh: Mesh): void{
+      this.shadowGenerator.addShadowCaster(mesh, [this.sun_light, this.moon_light])
+  }
 
   public update_player(uid: string, player: Player) {
     this.players.set(uid, player);
@@ -120,7 +123,18 @@ class State_machine {
   }
 
   public dropItem(item: PlayerItem) {
-    this.socket_ref.send(new Packet(PacketType.drop_item, [{ item: item, position: this._client.position }]))
+    let rep = { item: item, position: this._client.position }
+    this.items.push(rep)
+    this.socket_ref.send(new Packet(PacketType.drop_item, [...this.items]))
+  }
+
+  public pickupItem(item: PlayerItem) {
+    let rep = this.items.find((element) => element.item._name == item._name)
+    let index = this.items.findIndex((element) => element == rep)
+
+    this.items.splice(index, 1)
+
+    // this.socket_ref.send() // make a new packet method for picking up items, same-ish idea to this
   }
 }
 
