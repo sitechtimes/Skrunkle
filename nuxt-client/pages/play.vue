c77cfff77b7b364a78da8adcb6a4b082d5a1e734
achan2005 <78696065+achan2005@users.noreply.github.com>
2023-06-05 23:23:10 -0400

    popup

--- nuxt-client/pages/play.vue
+++ nuxt-client/pages/play.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="outer">
-    <LoadingBar class="loading" :percent="percent" :loadtext="loadtext" ref="load"/>
+    <LoadingBar
+      class="loading"
+      :percent="percent"
+      :loadtext="loadtext"
+      ref="load"
+    />
     <button v-if="vr" class="play" @click="play()">Enter VR Mode</button>
     <div id="debug">
       <p id="name"></p>
@@ -12,6 +17,25 @@
       <p id="PickupItem"></p>
       <p id="PickedupItem"></p>
       <p id="vr"></p>
+      <div class="instructionPopup" v-if="showPopup">
+        <h1>Welcome to Skrunkle!</h1>
+        <p>Instructions:</p>
+        <ul>
+          <li>Move with "W" "A" "S" "D"</li>
+          <li>Collect 10 Whalen sheeps 🐑</li>
+          <li>Beat the clock!⏰</li>
+        </ul>
+        <button class="button-54" role="button" @click="startGame">
+          Start Game
+        </button>
+      </div>
+      <div class="winPopup" v-if="showPopup2">
+        <h1>You won!</h1>
+        <p>Great job collecting</p>
+        <button class="button-54" role="button" @click="startGame">
+          Play again
+        </button>
+      </div>
     </div>
 
     <!-- <div id="chatIcon">
@@ -30,10 +54,7 @@
       </form>
     </div> -->
 
-    <canvas
-      id="renderCanvas"
-      ref="renderCanvas"
-    ></canvas>
+    <canvas id="renderCanvas" ref="renderCanvas"></canvas>
   </div>
 </template>
 
@@ -47,56 +68,69 @@ export default {
       world: undefined,
       percent: 0,
       loadtext: "Loading...",
-      vr: false
+      vr: false,
+      showPopup: false,
+      showPopup2: true,
     };
   },
-  mounted() { 
-    
+  mounted() {
     const canvas = this.$refs.renderCanvas;
-    const world = new World(<HTMLCanvasElement>canvas, this.$config.public, this.update_loading);
+    const world = new World(
+      <HTMLCanvasElement>canvas,
+      this.$config.public,
+      this.update_loading
+    );
     world.init();
     this.world = world;
 
-    window.addEventListener("resize", this.resize)
-    window.addEventListener("click", ()=>{canvas.requestFullscreen()}, {once: true})
+    window.addEventListener("resize", this.resize);
+    window.addEventListener(
+      "click",
+      () => {
+        canvas.requestFullscreen();
+      },
+      { once: true }
+    );
   },
   methods: {
-    resize(){
-      this.world.resize()
+    resize() {
+      this.world.resize();
+    },
+    startGame() {
+      this.showPopup = false;
     },
     sendChat() {
       let chat = this.world.chat;
-      console.log("register send")
+      console.log("register send");
       chat.sendMessage(this.chatMessage);
       this.chatMessage = undefined;
     },
-    update_loading(loaded, total, message){
-
-      if (message == "server"){
-        this.percent = 0
-        this.loadtext = "Fetching Server"
-      } else if (message == "meshes"){
-        this.percent = loaded/total
-        this.loadtext = `Creating Meshes (${loaded}/${total})`
+    update_loading(loaded, total, message) {
+      if (message == "server") {
+        this.percent = 0;
+        this.loadtext = "Fetching Server";
+      } else if (message == "meshes") {
+        this.percent = loaded / total;
+        this.loadtext = `Creating Meshes (${loaded}/${total})`;
       }
 
       if (this.percent == 1) {
-        this.vr = this.world.vr
-        if (!this.vr) this.play()
+        this.vr = this.world.vr;
+        if (!this.vr) this.play();
+        this.showPopup = true;
       }
-
     },
-    play(){
-      this.$refs['load'].load()
-      if (this.vr) this.world.enterVR()
-    }
+    play() {
+      this.$refs["load"].load();
+      if (this.vr) this.world.enterVR();
+    },
   },
 };
 </script>
 
 <style>
-
-.outer{
+@import url("https://fonts.googleapis.com/css2?family=Titan+One&display=swap");
+.outer {
   display: flex;
   width: 100vw;
   height: 100vh;
@@ -104,7 +138,7 @@ export default {
   align-items: center;
 }
 
-.play{
+.play {
   z-index: 1000;
   width: 20rem;
   height: 5rem;
@@ -116,7 +150,7 @@ export default {
   padding: 0;
   margin: 0;
 }
-canvas{
+canvas {
   z-index: 0;
   position: absolute;
   top: 0;
@@ -168,5 +202,52 @@ canvas{
   padding: 1rem;
   border-radius: 4rem;
 }
+.instructionPopup {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: orange;
+  color: black;
+  font-family: "Titan One", cursive;
+  box-shadow: 0 10px 10px rgba(33, 29, 29, 0.3);
+  text-align: center;
+  padding: 2rem;
+  border-radius: 2rem;
+  border: solid white 0.2rem;
+}
+h1 {
+  margin-bottom: 1rem;
+}
+ul {
+  margin-bottom: 1rem;
+}
 
+.button-54 {
+  font-size: 16px;
+  letter-spacing: 2px;
+  text-decoration: none;
+  text-transform: uppercase;
+  color: #000;
+  cursor: pointer;
+  border: 3px solid;
+  padding: 0.25em 0.5em;
+  box-shadow: 1px 1px 0px 0px, 2px 2px 0px 0px, 3px 3px 0px 0px, 4px 4px 0px 0px,
+    5px 5px 0px 0px;
+  position: relative;
+  touch-action: manipulation;
+  margin-top: 0.7rem;
+}
+
+.button-54:active {
+  box-shadow: 0px 0px 0px 0px;
+  top: 5px;
+  left: 5px;
+}
+
+@media (min-width: 768px) {
+  .button-54 {
+    padding: 0.25em 0.75em;
+  }
+}
 </style>
